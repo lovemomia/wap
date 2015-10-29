34dd6f234ebb70346deb6cdaa82477ed8b8a6f9a
吴秋锋 <arthas.wuqf@gmail.com>
2015-10-29 18:34:47 +0800

    支付验证页重构

--- src/main/java/cn/momia/wap/web/ctrl/payment/PaymentController.java
+++ src/main/java/cn/momia/wap/web/ctrl/payment/PaymentController.java
@@ -1,15 +1,12 @@
 package cn.momia.wap.web.ctrl.payment;
 
-import cn.momia.common.api.http.MomiaHttpResponse;
 import cn.momia.wap.web.ctrl.AbstractController;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,19 +22,5 @@ public class PaymentController extends AbstractController {
         params.put("fee", fee);
 
         return new ModelAndView("payment/pay", "params", params);
-    }
-
-    @RequestMapping(value = "/result", method = RequestMethod.GET)
-    public ModelAndView success(HttpServletRequest request, @RequestParam(value = "oid") long orderId) {
-        String utoken = getUtoken(request);
-        if (StringUtils.isBlank(utoken)) return new ModelAndView("forward:/auth/login");
-
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("utoken", utoken);
-        params.put("oid", String.valueOf(orderId));
-
-        MomiaHttpResponse resp = post("/payment/check", params);
-
-        return new ModelAndView("payment/result", "result", resp.getData());
     }
 }
--- web/WEB-INF/ftl/payment/result.ftl
+++ web/WEB-INF/ftl/payment/result.ftl
@@ -5,27 +5,11 @@
 </@override>
 
 <@override name="body">
-    <div class="header bg-white bottom-border"><div class="back left"><img src="/img/back3x.png"></div>
-        <#if (result.payed == true)>
-        购买成功
-        <#else>
-        付款失败
-        </#if>
-    </div>
+    <div class="header bg-white bottom-border"><div class="back left"><img src="/img/back3x.png"></div></div>
     <div class="content">
         <div class="logo"><img src="/img/logo3x.png"></div>
-        <#if (result.payed == true)>
-            <div class="title">购买成功</div>
-            <div class="desc">您已购买成功，请提前选课，并预约</div>
-            <div class="btn">
-                <div class="left"><button id="btn_booking" class="btn-orange">预约上课</button></div>
-                <div class="right"><button id="btn_order" class="btn-main">查看订单</button></div>
-                <div style="clear: both;"></div>
-            </div>
-        <#else>
-            <div class="title">付款失败</div>
-            <div class="desc">如果支付系统已扣款，请与客服联系</div>
-        </#if>
+        <div class="title"></div>
+        <div class="desc"></div>
     </div>
 </@override>
 
