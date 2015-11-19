e154357204c4419f13d0ec86aff8af34bc3759f2
吴秋锋 <arthas.wuqf@gmail.com>
2015-11-19 12:05:18 +0800

    展示方式调整

--- web/js/hongbao.js
+++ web/js/hongbao.js
@@ -17,11 +17,28 @@ $(function () {
                 alert("领取红包失败，无效的分享链接")
             } else {
                 var mobile = $("#mobile").val();
-                sg.common.post(sg.config.api + "/coupon/invite", {
-                    mobile: mobile,
-                    invite: inviteCode
-                }, function () {
-                    window.location.href = "/hongbao_result?mobile=" + mobile;
+                $.ajax({
+                    url: sg.config.api + "/coupon/invite",
+                    data: {
+                        mobile: mobile,
+                        invite: inviteCode
+                    },
+                    type: "post",
+                    dataType: "json",
+                    timeout: 5000,
+                    success: function (resp) {
+                        if (resp.errno != 0) {
+                            $(".msg").html(resp.errmsg);
+                            $("#mobile").val("");
+                            $("#get_coupon").removeClass("active");
+                            $("#get_coupon").addClass("disable");
+                        } else {
+                            window.location.href = "/hongbao_result?mobile=" + mobile;
+                        }
+                    },
+                    error: function () {
+                        alert("网络异常，请稍后再试");
+                    }
                 });
             }
         }
