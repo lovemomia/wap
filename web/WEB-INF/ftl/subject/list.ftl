37f31e18267df4716b7700852426b4956f5b183d
吴秋锋 <arthas.wuqf@gmail.com>
2016-01-21 15:07:12 +0800

    课程包列表页

---
+++ web/WEB-INF/ftl/subject/list.ftl
@@ -0,0 +1,16 @@
+<@override name="title">课程包列表</@override>
+
+<@override name="css">
+    <link rel="stylesheet" type="text/css" href="/css/subject/list.css">
+</@override>
+
+<@override name="body">
+<div class="header bg-white bottom-border"><div class="back left"><img src="/img/back3x.png"></div>课程包列表</div>
+<div class="content top-padding"></div>
+</@override>
+
+<@override name="js">
+    <script type="text/javascript" src="/js/subject/list.js"></script>
+</@override>
+
+<@extends name="../base.ftl"/>
---
+++ web/js/subject/list.js
@@ -0,0 +1,40 @@
+$(function () {
+    sg.subjectlist.all(sg.config.city);
+});
+
+sg.subjectlist = {
+    all: function (city) {
+        sg.common.get(sg.config.api + "/subject/list", {
+            city: city
+        }, sg.subjectlist.success);
+    },
+
+    success: function (list) {
+        if (list != undefined && list.length > 0) {
+            var html = "";
+            for (var i = 0; i < list.length; i++) {
+                html += generate_subject_html(list[i]);
+            }
+
+            $(".content").append(html);
+
+            function generate_subject_html(subject) {
+                var html = "";
+                if (sessionStorage.getItem("_src") != null) {
+                    html += "<a href='" + sg.config.appname + "://subjectdetail?id=" + subject.id + "'>";
+                } else {
+                    html += "<a href='/subjectdetail?id=" + subject.id + "'>";
+                }
+                html += "<div class='subject'>";
+                html += "<div class='cover' style='background-image: url(" + subject.cover + ")'></div>";
+                html += "<div class='desc'>";
+                html += "<div class='title overflow-hidden'>" + subject.title + "</div>";
+                html += "</div>";
+                html += "</div>";
+                html += "</a>";
+
+                return html;
+            }
+        }
+    }
+};
