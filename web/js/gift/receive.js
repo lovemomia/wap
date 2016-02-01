d68588ff5d8afe0ed4aab134b6333884d8faff25
吴秋锋 <arthas.wuqf@gmail.com>
2016-02-01 15:39:14 +0800

    登录后自动领取

--- web/js/gift/receive.js
+++ web/js/gift/receive.js
@@ -4,27 +4,37 @@ $(function () {
         window.location.href = "/";
     }
 
+    var receive = sg.common.param("receive", 0);
+    if (receive == 1 && sg.common.is_login()) sg.receive.receive();
+
     $("#btn_receive").on("click", function () {
         if (!sg.common.is_login()) {
-            alert("本新年礼包只有松果亲子的注册用户才能领取哦，您还没有登录，快去登录后再来领取吧~（未注册的用户需要先注册）")
-            sg.common.redirect_login();
+            alert("本新年礼包只有松果亲子的注册用户才能领取哦，您还没有登录，快去登录后再来领取吧~（未注册的用户需要先注册）");
+            var current_url = window.location.href + "&receive=1";
+            window.location.href = "/auth/login?ref=" + encodeURIComponent(current_url);
         } else {
-            var oid = sg.common.param("oid");
-            var expired = sg.common.param("expired");
-            var giftsign = sg.common.param("giftsign");
-            sg.common.post(sg.config.api + "/subject/order/gift/receive", {
-                utoken: sg.common.cookie.get("utoken"),
-                oid: oid,
-                expired: expired,
-                giftsign: giftsign
-            }, function (data) {
-                if (data == true) {
-                    alert("新年礼物领取成功，快去选课吧~");
-                    window.location.href = "/user/bookable";
-                } else {
-                    alert("新年礼物领取失败，请与客服联系");
-                }
-            });
+            sg.receive.receive();
         }
     });
 });
+
+sg.receive = {
+    receive: function () {
+        var oid = sg.common.param("oid");
+        var expired = sg.common.param("expired");
+        var giftsign = sg.common.param("giftsign");
+        sg.common.post(sg.config.api + "/subject/order/gift/receive", {
+            utoken: sg.common.cookie.get("utoken"),
+            oid: oid,
+            expired: expired,
+            giftsign: giftsign
+        }, function (data) {
+            if (data == true) {
+                alert("新年礼物领取成功，快去选课吧~");
+                window.location.href = "/user/bookable";
+            } else {
+                alert("新年礼物领取失败，请与客服联系");
+            }
+        });
+    }
+};
