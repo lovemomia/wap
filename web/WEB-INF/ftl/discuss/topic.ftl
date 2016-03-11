6e3457583004b431479d8e438f2bacd6488868bb
吴秋锋 <arthas.wuqf@gmail.com>
2016-03-11 15:16:19 +0800

    topic 页面调整

--- web/WEB-INF/ftl/discuss/topic.ftl
+++ web/WEB-INF/ftl/discuss/topic.ftl
@@ -6,51 +6,11 @@
 
 <@override name="body">
     <div class="header bg-white bottom-border"><div class="back left"><img src="/img/back3x.png"></div>话题讨论</div>
-    <div class="content bg-white has-fixed-footer bottom-border">
-        <div class="title">${topic.topic.title}</div>
-        <div class="body">${topic.topic.content}</div>
-        <div class="replies-title bottom-border">所有回答<span>${topic.replies.totalCount}</span></div>
-        <div class="new-reply"></div>
-        <#if (topic.replies.totalCount>0)>
-            <#list topic.replies.list as reply>
-                <div class="reply">
-                    <#if (reply_index>0)><hr class="left-margin" /></#if>
-                    <div class="reply-avatar left">
-                        <#if (reply.avatar?length>0)>
-                            <img src="${reply.avatar}" />
-                        <#else>
-                            <img src="/img/avatar02.png" />
-                        </#if>
-                    </div>
-                    <div class="reply-info right top-margin bottom-margin">
-                        <div>
-                            <div class="nickname left">${reply.nickName}</div>
-                            <div class="addtime right">${reply.addTime}</div>
-                            <div style="clear:both"></div>
-                        </div>
-                        <div>
-                            <#if (reply.children?size>0)>
-                                <div class="children left">${reply.children[0]}</div>
-                            </#if>
-                            <div style="clear:both"></div>
-                        </div>
-                        <div class="reply-content">${reply.content}</div>
-                        <div class="reply-star">
-                            <#if reply.stared==true>
-                                <img class="left star" src="/img/zan_active3x.png" reply-id="${reply.id}" stared="1" star-count="${reply.staredCount}" />
-                            <#else>
-                                <img class="left star" src="/img/zan3x.png" reply-id="${reply.id}" stared="0" star-count="${reply.staredCount}" />
-                            </#if>
-                            <span>${reply.staredCount}</span>
-                        </div>
-                    </div>
-                    <div style="clear:both"></div>
-                </div>
-            </#list>
-            <#if topic.replies.nextIndex??><div class="view-all top-border">查看更多</div></#if>
-        <#else>
-            <div class="tips"><p>目前还没有人参与讨论，快来发表下你的观点吧~</p></div>
-        </#if>
+    <div class="content bottom-border bg-white has-fixed-footer ">
+        <div id="title" class="title"></div>
+        <div id="body" class="body"></div>
+        <div class="replies-title bottom-border">所有回答<span id="totalCount">0</span></div>
+        <div class="new-reply bg-white"></div>
     </div>
     <div class="footer fixed">
         <button id="btn_join" class="btn-orange">参与讨论</button>
--- web/js/discuss/topic.js
+++ web/js/discuss/topic.js
@@ -1,39 +1,6 @@
 $(function () {
-    $(".star").on("click", function () {
-        if (!sg.common.is_login()) {
-            if (confirm("需要登录后才能操作，快去登录吧")) sg.common.redirect_login();
-        } else {
-            var reply_id = $(this).attr("reply-id");
-            var stared = $(this).attr("stared");
-            var star_count = $(this).attr("star-count");
-            var star = $(this);
-            if (stared == 1) {
-                sg.common.post(sg.config.api + "/discuss/reply/unstar", {
-                    utoken: sg.common.cookie.get("utoken"),
-                    replyid: reply_id
-                }, function () {
-                    star.attr("src", "/img/zan3x.png");
-                    star.attr("stared", 0);
-                    star.attr("star-count", Math.max(0, star_count - 1));
-                    star.next().html(Math.max(0, star_count - 1));
-                });
-            } else {
-                sg.common.post(sg.config.api + "/discuss/reply/star", {
-                    utoken: sg.common.cookie.get("utoken"),
-                    replyid: reply_id
-                }, function () {
-                    star.attr("src", "/img/zan_active3x.png");
-                    star.attr("stared", 1);
-                    star.attr("star-count", Math.max(0, star_count + 1));
-                    star.next().html(Math.max(0, star_count + 1));
-                });
-            }
-        }
-    });
-
-    $(".view-all").on("click", function () {
-        window.location.href = "/discuss/replies.ftl?topicid=" + sg.common.param("id");
-    });
+    var topicid = sg.common.param("id", 0);
+    sg.topic.more(topicid, 0);
 
     $("#btn_join").on("click", click_join);
 
@@ -71,6 +38,7 @@ $(function () {
             }, function () {
                 alert("发表成功");
                 $(".new-reply").append("<p>新发表：" + content + "</p>");
+                $("#no-reply").remove();
                 click_cancel();
             });
         }
@@ -84,3 +52,113 @@ $(function () {
         $("#btn_join").on("click", click_join);
     }
 });
+
+sg.topic = {
+    more: function (topicid, start) {
+        sg.common.get(sg.config.api + "/discuss/topic", {
+            utoken: sg.common.cookie.get("utoken"),
+            id: topicid,
+            start: start
+        }, sg.topic.success);
+    },
+
+    success: function (data) {
+        var topic = data.topic;
+        if (topic != undefined) {
+            $("#title").html(topic.title);
+            $("#body").html(topic.content);
+        }
+
+        var replies = data.topic;
+        var totalCount = replies.totalCount;
+        if (totalCount > 0) {
+            $("#totalCount").html(totalCount);
+            var list = replies.list;
+            sg.common.unbind_scrollin();
+
+            var html = "";
+            html += "<div class='list bottom-border'>";
+            for (var i = 0; i < list.length; i++) {
+                html += generate_replie_html(list[i]);
+                if (i < list.length - 1) html += "<hr class='left-margin' />";
+            }
+            html += "</div>";
+
+            $(".content").append(html);
+
+            if (replies.nextIndex != undefined) sg.common.bind_scrollin(function () {
+                sg.topic.more(sg.common.param("id"), replies.nextIndex);
+            });
+        } else {
+            var html = "";
+            html += "<div id='no-reply' class='tips'>";
+            html += "<p>目前还没有人参与讨论，快来发表下你的观点吧~</p>";
+            html += "</div>";
+
+            $(".content").append(html);
+        }
+
+        function generate_replie_html(reply) {
+            var html = "";
+
+            html += "<div class='reply scrollable'>";
+            html += "<div class='reply-avatar left'>";
+            if (reply.avatar.length> 0) html += "<img src='" + reply.avatar + "' />";
+            else html += "<img src='/img/avatar02.png' />";
+            html += "</div>";
+            html += "<div class='reply-info right top-margin bottom-margin'>";
+            html += "<div>";
+            html += "<div class='nickname left'>" + reply.nickName + "</div>";
+            html += "<div class='addtime right'>" + reply.addTime + "</div>";
+            html += "<div style='clear:both'></div>";
+            html += "</div>";
+            html += "<div>";
+            if (reply.children.length > 0) html += "<div class='children left'>" + reply.children[0] + "</div>";
+            html += "<div style='clear:both'></div>";
+            html += "</div>";
+            html += "<div class='reply-content'>" + reply.content + "</div>";
+            html += "<div class='reply-star'>";
+            if (reply.stared) html += "<img class='left star' src='/img/zan_active3x.png' reply-id='" + reply.id + "' stared='1' star-count='" + reply.staredCount + "' />";
+            else html += "<img class='left star' src='/img/zan3x.png' reply-id='" + reply.id + "' stared='0' star-count='" + reply.staredCount + "' />";
+            html += "<span>" + reply.staredCount + "</span>";
+            html += "</div>";
+            html += "</div>";
+            html += "<div style='clear:both'></div>";
+            html += "</div>";
+
+            return html;
+        }
+
+        $(".star").on("click", function () {
+            if (!sg.common.is_login()) {
+                if (confirm("需要登录后才能操作，快去登录吧")) sg.common.redirect_login();
+            } else {
+                var reply_id = $(this).attr("reply-id");
+                var stared = $(this).attr("stared");
+                var star_count = $(this).attr("star-count");
+                var star = $(this);
+                if (stared == 1) {
+                    sg.common.post(sg.config.api + "/discuss/reply/unstar", {
+                        utoken: sg.common.cookie.get("utoken"),
+                        replyid: reply_id
+                    }, function () {
+                        star.attr("src", "/img/zan3x.png");
+                        star.attr("stared", 0);
+                        star.attr("star-count", Math.max(0, star_count - 1));
+                        star.next().html(Math.max(0, star_count - 1));
+                    });
+                } else {
+                    sg.common.post(sg.config.api + "/discuss/reply/star", {
+                        utoken: sg.common.cookie.get("utoken"),
+                        replyid: reply_id
+                    }, function () {
+                        star.attr("src", "/img/zan_active3x.png");
+                        star.attr("stared", 1);
+                        star.attr("star-count", Math.max(0, star_count + 1));
+                        star.next().html(Math.max(0, star_count + 1));
+                    });
+                }
+            }
+        });
+    }
+};
